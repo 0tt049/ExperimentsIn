17461ef1f28389a19e0178f50233ac453b174ca7
Otavio Coelho <otavio.gmcoelho@gmail.com>
2023-04-19 00:31:45 -0300

    Made my first render

---
+++ Raytracing/raytracer.rb
@@ -0,0 +1,26 @@
+# [[file:../README.org::*First gradient in Ruby][First gradient in Ruby:1]]
+# Image
+
+image_width = 256
+image_height = 256
+
+hor = (0..(image_width - 1)).to_a
+ver = hor.reverse
+
+# Render
+$stdout << "P3\n" << image_width << " " << image_height << "\n255\n"
+
+ver.each do |ypx|
+  hor.each do |xpx|
+    r = xpx.to_f / (image_width - 1)
+    g = ypx.to_f / (image_height - 1)
+    b = 0.25
+
+    ir = (r * 255.999).to_i
+    ig = (g * 255.999).to_i
+    ib = (b * 255.999).to_i
+
+    $stdout << ir << " " << ig << " " << ib << "\n"
+  end
+end
+# First gradient in Ruby:1 ends here
